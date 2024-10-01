b1a2c0436c962e1b24433e66da3a4f25969ebc39
Bruno Feliciano <brunofelimarti@gmail.com>
2024-10-01 10:09:41 -0300

    rabbit

--- src/main/java/com/example/DevStore/DevStoreApplication.java
+++ src/main/java/com/example/DevStore/DevStoreApplication.java
@@ -1,11 +1,13 @@
 package com.example.DevStore;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableRabbit
 public class DevStoreApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/DevStore/rabbitMQ/SenderConfig.java
@@ -0,0 +1,16 @@
+package com.example.DevStore.rabbitMQ;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Queue;
+
+@Configuration
+public class SenderConfig {
+    @Value("${queue.name}")
+    private String message;
+    @Bean
+    public Queue queue() {
+        return new Queue(message, true);
+    }
+}
